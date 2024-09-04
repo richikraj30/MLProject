b945bbfc9d164066b0d1a8d45032850f2d27ea94
richikraj30 <richikraj30@gmail.com>
2024-09-04 19:05:58 +0530

    EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail:sys):
@@ -19,3 +19,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+

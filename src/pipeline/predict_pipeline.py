332c712fab45e4d0a1cfc142ed53465ca41e6d94
richikraj30 <richikraj30@gmail.com>
2024-09-10 20:57:30 +0530

    Deployment Configuration

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -98,22 +98,17 @@ class CustomData:
         writing_score: int):
 
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
+
                 "gender": [self.gender],
                 "race_ethnicity": [self.race_ethnicity],
                 "parental_level_of_education": [self.parental_level_of_education],
@@ -121,6 +116,7 @@ class CustomData:
                 "test_preparation_course": [self.test_preparation_course],
                 "reading_score": [self.reading_score],
                 "writing_score": [self.writing_score],
+                
             }
 
             return pd.DataFrame(custom_data_input_dict)
